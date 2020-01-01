9e5a81e5ca19f2979cc18e89f3be1f3daae2b966
Stephan Wink <stephan_wink@web.de>
2020-01-01 14:40:06 +0100

    add initial c templates to repository

---
+++ c-sources/template.c
@@ -0,0 +1,79 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2509
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;\red89\green138\blue67;\red23\green23\blue23;\red202\green202\blue202;
+}
+{\*\expandedcolortbl;;\cssrgb\c41569\c60000\c33333;\cssrgb\c11765\c11765\c11765;\cssrgb\c83137\c83137\c83137;
+}
+\paperw11900\paperh16840\margl1440\margr1440\vieww28600\viewh16580\viewkind0
+\deftab720
+\pard\pardeftab720\sl360\partightenfactor0
+
+\f0\fs24 \cf2 \cb3 \expnd0\expndtw0\kerning0
+/*****************************************************************************************\cf4 \cb1 \
+\cf2 \cb3 * FILENAME :        .c\cf4 \cb1 \
+\cf2 \cb3 *\cf4 \cb1 \
+\cf2 \cb3 * DESCRIPTION :\cf4 \cb1 \
+\cf2 \cb3 *       This module is a test\
+*\
+* AUTHOR :    Stephan Wink        CREATED ON :    24.01.2019\cf4 \cb1 \
+\cf2 \cb3 *\cf4 \cb1 \
+\cf2 \cb3 * PUBLIC FUNCTIONS :\cf4 \cb1 \
+\cf2 \cb3 *\cf4 \cb1 \
+\cf2 \cb3 * Copyright (c) [2017] [Stephan Wink]\cf4 \cb1 \
+\cf2 \cb3 *\cf4 \cb1 \
+\cf2 \cb3 * Permission is hereby granted, free of charge, to any person obtaining a copy\cf4 \cb1 \
+\cf2 \cb3 * of this software and associated documentation files (the "Software"), to deal\cf4 \cb1 \
+\cf2 \cb3 * in the Software without restriction, including without limitation the rights\cf4 \cb1 \
+\cf2 \cb3 * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\cf4 \cb1 \
+\cf2 \cb3 * copies of the Software, and to permit persons to whom the Software is\cf4 \cb1 \
+\cf2 \cb3 * furnished to do so, subject to the following conditions:\cf4 \cb1 \
+\cf2 \cb3 *\cf4 \cb1 \
+\cf2 \cb3 * The above copyright notice and this permission notice shall be included in all\cf4 \cb1 \
+\cf2 \cb3 * copies or substantial portions of the Software.\cf4 \cb1 \
+\cf2 \cb3 *\cf4 \cb1 \
+\cf2 \cb3 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\cf4 \cb1 \
+\cf2 \cb3 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\cf4 \cb1 \
+\cf2 \cb3 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\cf4 \cb1 \
+\cf2 \cb3 vAUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\cf4 \cb1 \
+\cf2 \cb3 * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\cf4 \cb1 \
+\cf2 \cb3 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\cf4 \cb1 \
+\cf2 \cb3 * SOFTWARE.\cf4 \cb1 \
+\cf2 \cb3 *\cf4 \cb1 \
+\cf2 \cb3 *****************************************************************************************/\cf4 \cb1 \
+\
+\cf2 \cb3 /****************************************************************************************/\cf4 \cb1 \
+\cf2 \cb3 /* Include Interfaces */\cf4 \cb1 \
+\
+\cf2 \cb3 /****************************************************************************************/\cf4 \cb1 \
+\cf2 \cb3 /* Local constant defines */\cf4 \cb1 \
+\
+\cf2 \cb3 /****************************************************************************************/\cf4 \cb1 \
+\cf2 \cb3 /* Local function like makros */\cf4 \cb1 \
+\
+\cf2 \cb3 /****************************************************************************************/\cf4 \cb1 \
+\cf2 \cb3 /* Local type definitions (enum, struct, union) */\
+\
+/****************************************************************************************/\cf4 \cb1 \
+\cf2 \cb3 /* Local functions prototypes: */\
+\
+/****************************************************************************************/\cf4 \cb1 \
+\cf2 \cb3 /* Local variables: */\cf4 \cb1 \
+\
+\cf2 \cb3 /****************************************************************************************/\cf4 \cb1 \
+\cf2 \cb3 /* Global functions (unlimited visibility) */\
+\
+/**---------------------------------------------------------------------------------------\cf4 \cb1 \
+\cf2 \cb3  * @brief     Constructor for Bme280Sensor\cf4 \cb1 \
+\cf2 \cb3  * @author    S. Wink\cf4 \cb1 \
+\cf2 \cb3  * @date      24. Jan. 2019\cf4 \cb1 \
+\cf2 \cb3  * @param     p_trace             trace object for info and error messages\cf4 \cb1 \
+\cf2 \cb3  * @param     bmePwr_p            pointer to power gpio pin\cf4 \cb1 \
+\cf2 \cb3  * @param     bmeStat_p           pointer to bme sensor status pin (power up)\cf4 \cb1 \
+\cf2 \cb3  * @return    n/a\cf4 \cb1 \
+\cf2 \cb3 *//*-----------------------------------------------------------------------------------*/\
+\
+/****************************************************************************************/\cf4 \cb1 \
+\cf2 \cb3 /* Local functions: */\cf4 \cb1 \
+\
+\
+}
---
+++ c-sources/template.h
@@ -0,0 +1,58 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1671\cocoasubrtf400
+{\fonttbl\f0\fnil\fcharset0 Monaco;}
+{\colortbl;\red255\green255\blue255;\red63\green127\blue95;}
+{\*\expandedcolortbl;;\csgenericrgb\c24706\c49804\c37255;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww21420\viewh15280\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs28 \cf2 /*****************************************************************************************\cf0 \
+\cf2 * FILENAME :        controlTask.h\cf0 \
+\cf2 *\cf0 \
+\cf2 * DESCRIPTION :\cf0 \
+\cf2 *       Header file for \cf0 \
+\cf2 *\cf0 \
+\cf2 * Date: 24. January 2019\cf0 \
+\cf2 *\cf0 \
+\cf2 * NOTES :\cf0 \
+\cf2 *\cf0 \
+\cf2 * Copyright (c) [2019] [\ul Stephan\ulnone  Wink]\cf0 \
+\cf2 *\cf0 \
+\cf2 * Permission is hereby granted, free of charge, to any person obtaining a copy\cf0 \
+\cf2 * of this software and associated documentation files (the "Software"), to deal\cf0 \
+\cf2 * in the Software without restriction, including without limitation the rights\cf0 \
+\cf2 * to use, copy, modify, merge, publish, distribute, \ul sublicense\ulnone , and/or sell\cf0 \
+\cf2 * copies of the Software, and to permit persons to whom the Software is\cf0 \
+\cf2 * furnished to do so, subject to the following conditions:\cf0 \
+\cf2 *\cf0 \
+\cf2 * The above copyright notice and this permission notice shall be included in all\cf0 \
+\cf2 * copies or substantial portions of the Software.\cf0 \
+\cf2 *\cf0 \
+\cf2 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\cf0 \
+\cf2 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\cf0 \
+\cf2 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\cf0 \
+\cf2 vAUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\cf0 \
+\cf2 * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\cf0 \
+\cf2 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\cf0 \
+\cf2 * SOFTWARE.\cf0 \
+\cf2 *****************************************************************************************/\cf0 \
+\
+\cf2 /****************************************************************************************/\cf0 \
+\cf2 /* Imported header files: */\cf0 \
+\
+\cf2 /****************************************************************************************/\cf0 \
+\cf2 /* Global constant defines: */\cf0 \
+\
+\cf2 /****************************************************************************************/\cf0 \
+\cf2 /* Global function like macro defines (to be avoided): */\cf0 \
+\
+\cf2 /****************************************************************************************/\cf0 \
+\cf2 /* Global type definitions (\ul enum\ulnone  (\ul en\ulnone ), \ul struct\ulnone  (\ul st\ulnone ), union (\ul un\ulnone ), typedef (\ul tx\ulnone ): */\cf0 \
+\
+\cf2 /****************************************************************************************/\cf0 \
+\cf2 /* Global function definitions: */\cf0 \
+\
+\cf2 /****************************************************************************************/\cf0 \
+\cf2 /* Global data definitions: */\cf0 \
+\
+}
